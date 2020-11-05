a937a7c21db02dcef9bb2495935bf73078b5ed16
MahmoudGhnnam <mahmou999@gmail.com>
2020-11-05 17:29:47 +0200

    changed

--- server.js
+++ server.js
@@ -3,13 +3,15 @@
 // const  static  = require('express');
 const express = require('express');
 const app = express();
-const PORT = 3030;
+const PORT = 8000;
 
 app.use(express.static('./puplic'));
 
 // app.get('/',(request, reponse )=>{
 //    reponse.status(200).send('Hello there');
 // })
+
+app.use('*', (request, response) => response.send('sorry'));
 
 app.get('/about',(request, reponse )=>{
     reponse.status(200).send('Hello there and gg');
